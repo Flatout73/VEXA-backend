b0647621bdd895a16d21139b40cb7265c0c6b25d
Leonid Liadveikin <fantom2259@gmail.com>
2022-06-14 00:03:41 +0200

    heroku database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -8,13 +8,19 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vexa",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_vexa_password",
-        database: Environment.get("DATABASE_NAME") ?? "vexa"
-    ), as: .psql)
+    if let databaseURL = Environment.get("DATABASE_URL"), var postgresConfig = PostgresConfiguration(url: databaseURL) {
+        postgresConfig.tlsConfiguration = .makeClientConfiguration()
+        postgresConfig.tlsConfiguration?.certificateVerification = .none
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vexa",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_vexa_password",
+            database: Environment.get("DATABASE_NAME") ?? "vexa"
+        ), as: .psql)
+    }
 
     app.migrations.add([CreateUser(), CreateAmbassador(), CreateContent()])
 
