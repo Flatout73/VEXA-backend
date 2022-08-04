a0fa42d5c92a85479246b1591691ab9dd8f07968
Leonid Liadveikin <fantom2259@gmail.com>
2022-08-04 03:25:28 +0200

    Update student info

--- Sources/App/Controllers/StudentController.swift
+++ Sources/App/Controllers/StudentController.swift
@@ -4,6 +4,12 @@ import SwiftProtobuf
 import Protobuf
 
 struct StudentController: RouteCollection {
+    var protoConfig = JSONDecodingOptions()
+
+    init() {
+        protoConfig.ignoreUnknownFields = true
+    }
+
     func boot(routes: RoutesBuilder) throws {
         let users = routes.grouped("students")
         users.get(":x", use: index)
@@ -14,6 +20,7 @@ struct StudentController: RouteCollection {
 
         users.group(UserAuthenticator()) { authenticated in
             authenticated.get("me", use: getCurrentUser)
+            authenticated.put("update", use: updateAdditionalInfo)
         }
     }
 
@@ -43,6 +50,37 @@ struct StudentController: RouteCollection {
         }
 
         return Proto(from: student)
+    }
+
+    func updateAdditionalInfo(_ req: Request) async throws -> StudentModel {
+        let payload = try req.auth.require(SessionJWTToken.self)
+
+        guard let user = try await req.users
+            .find(id: payload.userID) else {
+            throw AuthenticationError.userNotFound
+        }
+
+        guard let content = req.body.string else {
+            throw Abort(.badRequest)
+        }
+        print(content)
+        let updateRequest = try StudentInfoRequest(jsonString: content, options: protoConfig)
+
+        try await user.$student.load(on: req.db)
+        guard let student = user.student else {
+            throw AuthenticationError.userNotFound
+        }
+
+        student.nativeLanguage = updateRequest.nativeLanguage
+        student.bio = updateRequest.bio
+        student.currentCountry = updateRequest.currentCountry
+        student.otherLanguages = updateRequest.otherLanguages
+        student.enrollmentYear = updateRequest.enrollmentYear
+        student.dateOfBirthday = updateRequest.dateOfBirth.date
+
+        try await student.save(on: req.db)
+
+        return student
     }
     
 //    func delete(req: Request) async throws -> HTTPStatus {
